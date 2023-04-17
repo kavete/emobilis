be8b6659f4a7dab12408434f9c618a63c7deada3
kavete <kavetebrian@gmail.com>
2023-04-17 10:03:52 +0300

    First commit for gitProject

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF_8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<title> </title>
+	</head>
+	<body>
+		<h1>Hello Git </h1>
+	</body>
+</html>
